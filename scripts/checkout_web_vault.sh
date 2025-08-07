fe8f9612875e5bc9f1e2ab1b92002ea243f1e0ec
qbarbe <105909736+qbarbe@users.noreply.github.com>
2025-08-07 15:03:02 +0200

    set own repo for vw web build

--- scripts/checkout_web_vault.sh
+++ scripts/checkout_web_vault.sh
@@ -49,12 +49,12 @@ else
         VAULTWARDEN_REMOTE=$(git remote get-url vaultwarden || echo -n )
         if [ "x${VAULTWARDEN_REMOTE}" = "x" ]
 	then
-           echo "adding vaultwarden/vw_web_builds as remote repository"
-           git remote add vaultwarden https://github.com/vaultwarden/vw_web_builds.git
-        elif [ "${VAULTWARDEN_REMOTE}" != "https://github.com/vaultwarden/vw_web_builds.git" ]
+           echo "adding qbarbe/vw_web_builds as remote repository"
+           git remote add vaultwarden https://github.com/qbarbe/vw_web_builds.git
+        elif [ "${VAULTWARDEN_REMOTE}" != "https://github.com/qbarbe/vw_web_builds.git" ]
 	then
             echo "Warning: \`git remote get vaultwarden\` did not return the expected repository"
-	    echo "expected: https://github.com/vaultwarden/vw_web_builds.git"
+	    echo "expected: https://github.com/qbarbe/vw_web_builds.git"
 	    echo "received: ${VAULTWARDEN_REMOTE}"
 	    read -p "Press enter to continue"
         fi
